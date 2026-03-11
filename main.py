891dc4c2d598eb861c007d0017dcbbdd362af2c2
siwadol rukchart <hikiyasabuno@gmail.com>
2026-03-12 03:36:45 +0700

    add some window

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello team")
+import turtle as tt
+
+
+def main():
+    sc = tt.Screen()
+    sc.setup(width=800,height=500)
+    sc.bgcolor("pink")
+
+
+
+
+    return 0
+
+
+main()
+tt.done()
